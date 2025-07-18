2dff97d6ff3a574bc2ae74fd9abe687e8f30dfb0
Tak Igg <tigertak200@gmail.com>
2025-07-18 16:19:08 +1000

    added texts that show the number of books in each page

--- multi_page_scraper.py
+++ multi_page_scraper.py
@@ -2,7 +2,7 @@ import requests #access web pages on the internet
 from bs4 import BeautifulSoup #analyse and extract certain info on the HTML
 import pandas as pd #convert data into dataframe(表形式) and save as csv
 import matplotlib.pyplot as plt #data visualisation
-import seaborn as sns   #for grapgh design
+import seaborn as sns   #for graphic design of the charts
 
 #specify base url                               #👇put {} to change page number
 base_url = "https://books.toscrape.com/catalogue/page-{}.html"
@@ -13,17 +13,21 @@ all_data = []
 #scrape 1~3pages
 for page in range(1,4):
     # create the url of the current page
-    url = base_url.format(page) #input the numbers in the range in rhe base_url
+    url = base_url.format(page) 
     print(f"Fetching: {url}")
-    #Acquire the content of a web page
+    #Acquire the content of the web page
     res = requests.get(url)
     #Convert HTML into a readable format
     soup = BeautifulSoup(res.content, "html.parser")
     
-    #Acquire all the item blocks on a page
+    #Acquire all the book blocks on a page
     books = soup.select("article.product_pod")
-
-    #Acquire each book information from the blocks
+    #check the page number and the number of books on the page
+    if not books:
+        print(f"No books found on page {page}.")
+        continue  # Skip to the next page if no books are found
+    print(f"Page {page} has {len(books)} books.")    
+   #Acquire each book information from the blocks
     for book in books:
         title = book.h3.a["title"]
         price = book.select_one(".price_color").text
@@ -34,6 +38,12 @@ for page in range(1,4):
             "Price": price,
             "Availability": availability 
         })
+    #print the number of books scraped from the current page
+    print(
+        f"Page{page} scraped successfully! "
+        f"{len(books)} books added to the data array."
+    )
+
 
 # crate dateframe and Save as CSV
 df = pd.DataFrame(all_data)
